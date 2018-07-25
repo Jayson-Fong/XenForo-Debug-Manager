9ab459ac8b75938fcd4fc07c3573010f6ccd4ced
Jayson Fong <32722526+Jayson-Fong@users.noreply.github.com>
2018-07-25 16:50:36 -0400

    Fixed Unset Variable Issue

--- upload/library/jayson/DGM/Extend/XenForo/ControllerAdmin/Home.php
+++ upload/library/jayson/DGM/Extend/XenForo/ControllerAdmin/Home.php
@@ -3,7 +3,7 @@ class jayson_DGM_Extend_XenForo_ControllerAdmin_Home extends XFCP_jayson_DGM_Ext
     public function actionIndex() {
         $parent = parent::actionIndex();
         $debugModeStatus = XenForo_Application::getSimpleCacheData('debugModeStatus');
-        $parent->params['debugStatus'] = array('admin' => $debugModeStatus['admin'], 'public' => $debugModeStatus['public']);
+        $parent->params['debugStatus'] = $debugModeStatus;
         return $parent;
     }
 }
