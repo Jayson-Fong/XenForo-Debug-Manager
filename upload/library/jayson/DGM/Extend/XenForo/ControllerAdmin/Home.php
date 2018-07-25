64e8b748e4d416fb60fe0d0c2e3d24f91bfeb442
Jayson Fong <32722526+Jayson-Fong@users.noreply.github.com>
2018-07-25 13:39:55 -0400

    Combined Simple Cache Data

--- upload/library/jayson/DGM/Extend/XenForo/ControllerAdmin/Home.php
+++ upload/library/jayson/DGM/Extend/XenForo/ControllerAdmin/Home.php
@@ -2,10 +2,8 @@
 class jayson_DGM_Extend_XenForo_ControllerAdmin_Home extends XFCP_jayson_DGM_Extend_XenForo_ControllerAdmin_Home {
     public function actionIndex() {
         $parent = parent::actionIndex();
-        $admin = XenForo_Application::getSimpleCacheData('debugAdmin');
-        $public = XenForo_Application::getSimpleCacheData('debugPublic');
-        $adminRestrict = XenForo_Application::getSimpleCacheData('debugAdminRestrict');
-        $parent->params['debugStatus'] = array('admin' => $admin, 'public' => $public);
+        $debugModeStatus = XenForo_Application::getSimpleCacheData('debugModeStatus');
+        $parent->params['debugStatus'] = array('admin' => $debugModeStatus['admin'], 'public' => $debugModeStatus['public']);
         return $parent;
     }
 }
